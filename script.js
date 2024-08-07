b1d6a00f0e4de9db5aeea1a377fa61a4fad091ff
Praise Fakorede <praisefakoo2@gmail.com>
2024-08-07 18:41:55 +0000

    made first conditional

---
+++ script.js
@@ -0,0 +1,9 @@
+const fare = 2.75;
+let metroCard = 5.00; 
+
+
+if (metroCard >= fare) {
+    console.log("You have enough funds to go through.");
+} else {
+    console.log("Insufficient funds. Please add more money to your metro card.");
+}
